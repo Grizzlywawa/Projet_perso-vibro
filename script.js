978bf3d925630216db1f9859f5f08e6155a2e04c
Grizzlywawa <guerbeau-gicquel.pierre@orange.fr>
2023-03-03 10:08:50 +0100

    yes

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function runOnScroll() {
       }
  }; 
 window.addEventListener("scroll", runOnScroll);
+
+
+const slide = ["assets/images/vibro-slider-1.jpg", "assets/images/vibro-slider-3.jpg", "assets/images/vibro-slider-4.jpg", "assets/images/vibro-slider-5.jpg"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
